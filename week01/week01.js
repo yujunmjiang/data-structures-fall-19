69bd96b06bc2358f667e5e8aabd4ae377783ba58
Yujun Jiang <yujunmjiang@gmail.com>
2019-09-07 00:24:27 -0400

    Update week01.js

--- week01/week01.js
+++ week01/week01.js
@@ -2,8 +2,6 @@
 // Data Structure Weekly Assignment 01
 // Make a request for each of the ten "Meeting List Agenda" pages for Manhattan
 
-// Defines a constant reference to a value and change the properties of constant objects
-
 var request = require('request');
 var fs = require('fs');
 
@@ -19,6 +17,8 @@ var urls = [
     'https://parsons.nyc/aa/m09.html',
     'https://parsons.nyc/aa/m10.html'
 ];
+
+// Defines a constant reference to a value and change the properties of constant objects
 
 const linkPath = '/home/ec2-user/environment/week01/data/';
 const fns = ['m01.txt', 'm02.txt', 'm03.txt', 'm04.txt', 'm05.txt', 'm06.txt', 'm07.txt', 'm08.txt', 'm09.txt', 'm10.txt'];
