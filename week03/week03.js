b45de65e6beb5d733247fa76adda62d999b9322e
Yujun Jiang <yujunmjiang@gmail.com>
2019-09-15 19:14:16 -0400

    Update week03.js

--- week03/week03.js
+++ week03/week03.js
@@ -1,27 +1,35 @@
-// dependencies
+// Yujun Jiang
+// Data Structure Weekly Assignment 03
+// Makes a request to the Texas A&M Geoservices Geocoding APIs for each address
+
 var request = require('request'); // npm install request
 var async = require('async'); // npm install async
 var fs = require('fs');
 const dotenv = require('dotenv'); // npm install dotenv
 
 // TAMU api key
+
 dotenv.config();
 const apiKey = process.env.TAMU_KEY;
 
 // Read raw address data from a new JSON file
+
 var rawData = fs.readFileSync('../week03/data/data-m03-update.json');
 rawData = JSON.parse(rawData);
 
 // Geocode addresses
+
 var dataManhattan = [];
 var addresses = [];
 
 // Add all elements to the end of an array, 'addresses'
+
 for (var i = 0; i<rawData.length; i++) {
 addresses.push(rawData[i]['streetAddress']);
 }
 
 // 'eachSeries' in the async module iterates over an array and operates on each item in the array in series
+
 async.eachSeries(addresses, function(value, callback) {
     var apiRequest = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?';
     apiRequest += 'streetAddress=' + value.split(' ').join('%20');
