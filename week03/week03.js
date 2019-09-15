1fd1523aa4cd4e09b374d9797456751c485ae294
Yujun Jiang <yujunmjiang@gmail.com>
2019-09-15 18:56:11 -0400

    Add files via upload

---
+++ week03/week03.js
@@ -0,0 +1,45 @@
+// dependencies
+var request = require('request'); // npm install request
+var async = require('async'); // npm install async
+var fs = require('fs');
+const dotenv = require('dotenv'); // npm install dotenv
+
+// TAMU api key
+dotenv.config();
+const apiKey = process.env.TAMU_KEY;
+
+// Read raw address data from a new JSON file
+var rawData = fs.readFileSync('../week03/data/data-m03-update.json');
+rawData = JSON.parse(rawData);
+
+// Geocode addresses
+var dataManhattan = [];
+var addresses = [];
+
+// Add all elements to the end of an array, 'addresses'
+for (var i = 0; i<rawData.length; i++) {
+addresses.push(rawData[i]['streetAddress']);
+}
+
+// 'eachSeries' in the async module iterates over an array and operates on each item in the array in series
+async.eachSeries(addresses, function(value, callback) {
+    var apiRequest = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?';
+    apiRequest += 'streetAddress=' + value.split(' ').join('%20');
+    apiRequest += '&city=New%20York&state=NY&apikey=' + apiKey;
+    apiRequest += '&format=json&version=4.01';
+    
+    request(apiRequest, function(err, resp, body) {
+        if (err) {throw err;}
+        else {
+            var tamuGeo = JSON.parse(body);
+            console.log(tamuGeo['FeatureMatchingResultType']);
+            dataManhattan.push(tamuGeo);
+        }
+    });
+    setTimeout(callback, 2000);
+}, function() {
+    fs.writeFileSync('../week03/data/first.json', JSON.stringify(dataManhattan));
+    console.log('*** *** *** *** ***');
+    console.log('Number of meetings in this zone: ');
+    console.log(dataManhattan.length);
+});
