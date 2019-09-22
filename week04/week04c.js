80c2fc2e1ec5863637a7ca5b468f5f12b5bc1bb4
Yujun Jiang <yujunmjiang@gmail.com>
2019-09-22 19:14:56 -0400

    Add files via upload

---
+++ week04/week04c.js
@@ -0,0 +1,38 @@
+const { Client } = require('pg');
+var async = require('async');
+var fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'yujunmjiang';
+db_credentials.host = 'data-structures.cmqqziujkrxh.us-east-1.rds.amazonaws.com';
+db_credentials.database = 'aa';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+// Read raw address data from a new JSON file
+var rawData = fs.readFileSync('../week04/data/first.json');
+addressesForDb = JSON.parse(rawData);
+
+var addressesForDb = [];
+
+async.eachSeries(addressesForDb, function(value, callback) {
+
+    // Connect to the AWS RDS Postgres database
+    const client = new Client(db_credentials);
+    client.connect();
+
+    // Sample SQL statement to query the entire contents of a table: 
+    var thisQuery = "SELECT * FROM aalocations;";
+    // Sample SQL statement to delete a table: 
+    // var thisQuery = "DROP TABLE aalocations;";
+
+    console.log(thisQuery);
+    client.query(thisQuery, (err, res) => {
+        console.log(err, res);
+        client.end();
+    });
+    setTimeout(callback, 1000);
+});
