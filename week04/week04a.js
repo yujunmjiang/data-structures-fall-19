5b6901050daa86637e96ca165ab6172368c1ef8f
Yujun Jiang <yujunmjiang@gmail.com>
2019-09-24 01:39:03 -0400

    Update week04a.js

--- week04/week04a.js
+++ week04/week04a.js
@@ -15,9 +15,9 @@ const client = new Client(db_credentials);
 client.connect();
 
 // // Sample SQL statement to create a table: 
-var thisQuery = "CREATE TABLE aalocations (address varchar(100), lat double precision, long double precision);";
+var thisQuery = "CREATE TABLE locationInfo (address varchar(100), lat double precision, long double precision);";
 // Sample SQL statement to delete a table: 
-// var thisQuery = "DROP TABLE aalocations;"; 
+// var thisQuery = "DROP TABLE locationInfo;"; 
 
 client.query(thisQuery, (err, res) => {
     console.log(err, res);
