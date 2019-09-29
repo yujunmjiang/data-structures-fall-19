ac934a8b1542062aac93fb048fe492803dd9cc30
Yujun Jiang <yujunmjiang@gmail.com>
2019-09-29 17:38:44 -0400

    Update week04a.js

--- week04/week04a.js
+++ week04/week04a.js
@@ -1,8 +1,12 @@
+// Yujun Jiang
+// Data Structure Weekly Assignment 04
+
 const { Client } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
 // AWS RDS POSTGRESQL INSTANCE
+
 var db_credentials = new Object();
 db_credentials.user = 'yujunmjiang';
 db_credentials.host = 'data-structures.cmqqziujkrxh.us-east-1.rds.amazonaws.com';
@@ -11,10 +15,12 @@ db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = 5432;
 
 // Connect to the AWS RDS Postgres database
+
 const client = new Client(db_credentials);
 client.connect();
 
 // // Sample SQL statement to create a table: 
+
 var thisQuery = "CREATE TABLE locationInfo (address varchar(100), lat double precision, long double precision);";
 // Sample SQL statement to delete a table: 
 // var thisQuery = "DROP TABLE locationInfo;"; 
