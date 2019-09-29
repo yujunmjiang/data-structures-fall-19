4cb47708d91ae7d0c7b8e2e5cd5ef0879a41fe4b
Yujun Jiang <yujunmjiang@gmail.com>
2019-09-29 17:39:37 -0400

    Update week04b.js

--- week04/week04b.js
+++ week04/week04b.js
@@ -1,3 +1,6 @@
+// Yujun Jiang
+// Data Structure Weekly Assignment 03
+
 const { Client } = require('pg');
 var async = require('async');
 var fs = require('fs');
@@ -5,6 +8,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // AWS RDS POSTGRESQL INSTANCE
+
 var db_credentials = new Object();
 db_credentials.user = 'yujunmjiang';
 db_credentials.host = 'data-structures.cmqqziujkrxh.us-east-1.rds.amazonaws.com';
@@ -13,6 +17,7 @@ db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = 5432;
 
 // Read raw address data from a new JSON file
+
 var rawData = fs.readFileSync('../week04/data/first.json');
 addressesForDb = JSON.parse(rawData);
 
@@ -21,10 +26,12 @@ var addressesForDb = [];
 async.eachSeries(addressesForDb, function(value, callback) {
 
     // Connect to the AWS RDS Postgres database
+    
     const client = new Client(db_credentials);
     client.connect();
 
     // Sample SQL statement to create a table: 
+    
     var thisQuery = "INSERT INTO locationInfo VALUES (E'" + value.address + "', " + value.latLon.Latitude + ", " + value.latLon.Longitude + ");";
     // Sample SQL statement to delete a table: 
     // var thisQuery = "DROP TABLE locationInfo;";
