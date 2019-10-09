24120d14348aa2a10da4f1f6cb79bbbd3072f1f9
Yujun Jiang <yujunmjiang@gmail.com>
2019-10-09 18:29:27 -0400

    Update week02.js

--- week02/week02.js
+++ week02/week02.js
@@ -6,26 +6,21 @@ var fs = require('fs');
 var cheerio = require('cheerio');
 
 // Load the AA text file from week01 into a variable, `dataset`
-
 var dataset = fs.readFileSync('/home/ec2-user/environment/week01/data/m03.txt');
 
 // Load `dataset` into a cheerio object
-
 var $ = cheerio.load(dataset);
 
 // Write the project titles to a text file
-
 var dataManhattan = '';
 
 // Select tag and use attribute to narrow down the requested data
-
 $('td').each(function(i, elem) {
     if ($(elem).attr("style") == "border-bottom:1px solid #e3e3e3; width:260px") {
         dataManhattan += ($(elem).text()).trim() + '\n';
     }
     
 // Remove all unnecessary content by tag
-    
     $('b, div, span').remove();
 });
 
