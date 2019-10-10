47d5e52ccf700dac4d5d32f1cdb978e4fbe6aa80
Yujun Jiang <yujunmjiang@gmail.com>
2019-10-10 17:04:30 -0400

    Add files via upload

---
+++ week06/week06-aa.js
@@ -0,0 +1,31 @@
+// Yujun Jiang
+// Data Structure Weekly Assignment 06
+
+const { Client } = require('pg');
+const cTable = require('console.table');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'yujunmjiang';
+db_credentials.host = 'data-structures.cmqqziujkrxh.us-east-1.rds.amazonaws.com';
+db_credentials.database = 'aa';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+// Connect to the AWS RDS Postgres database
+const client = new Client(db_credentials);
+client.connect();
+// console.log(client);
+
+// Sample SQL statement to query meetings on Monday that start on or after 7:00pm: 
+var thisQuery = "SELECT address, lat, long FROM locationInfo;";
+
+client.query(thisQuery, (err, res) => {
+    if (err) {throw err}
+    else {
+        console.table(res.rows);
+        client.end();
+    }
+});
---
+++ week06/week06-dd.js
@@ -0,0 +1,34 @@
+// Yujun Jiang
+// Data Structure Weekly Assignment 06
+
+// npm install aws-sdk
+var AWS = require('aws-sdk');
+AWS.config = new AWS.Config();
+AWS.config.region = "us-east-1";
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName : "watch-history",
+    KeyConditionExpression: "#tp = :categoryName and #dt between :minDate and :maxDate", // the query expression
+    ExpressionAttributeNames: { // name substitution, used for reserved words in DynamoDB
+        "#tp" : "category",
+        "#dt" : "date"
+    },
+    ExpressionAttributeValues: { // the query values
+        ":categoryName": {S: "Action"},
+        ":minDate": {S: new Date("January 1, 2019").toISOString()},
+        ":maxDate": {S: new Date("December 31, 2019").toISOString()}
+    }
+};
+
+dynamodb.query(params, function(err, data) {
+    if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Query succeeded.");
+        data.Items.forEach(function(item) {
+            console.log("***** ***** ***** ***** ***** \n", item);
+        });
+    }
+});
